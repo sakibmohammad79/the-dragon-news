e5db5f435b21a18c4218ee372b22bcd4f1c09f11
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-04-27 18:11:19 +0600

    right and left navigation are complete

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [catagories, setCatagories] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/catagories')
+        .then(res => res.json())
+        .then(data => setCatagories(data))
+        .catch(error => console.error(error))
+    },[])
     return (
-        <div>
-            <h2>left navigation</h2>
+        <div className='mt-5'>
+            <h4>All Catagories</h4>
+            <h6>International News</h6>
+            <div className='ps-3'>
+            {
+                catagories.map((catagorie) => <p
+                kay={catagorie.id}
+                >
+                    <Link to={`/catagorie/${catagorie.id}`} className='text-decoration-none text-black'>{catagorie.name}</Link>
+                </p>)
+            }
+            </div>
         </div>
     );
 };
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right navigation</h2>
-        </div>
-    );
+  return (
+    <div className="mt-5">
+      <h4>Login With</h4>
+      <Button className="mb-2 px-3" variant="outline-primary">
+        {" "}
+        <FaGoogle /> Login With Google
+      </Button>
+      <Button className="px-3" variant="outline-secondary">
+        {" "}
+        <FaGithub /> Login With Github
+      </Button>
+      <div className="mt-3">
+        <ListGroup>
+            <h4>Find Us On</h4>
+          <ListGroup.Item> <FaFacebook/> Facbook</ListGroup.Item>
+          <ListGroup.Item> <FaTwitter/> Twitter</ListGroup.Item>
+          <ListGroup.Item> <FaInstagram/> Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
